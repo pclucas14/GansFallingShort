fcc4c8e229265e8e52a62f79697dd6caecbce7c8
Lucas PAGE-CACCIA <lucas.page-caccia@mail.mcgill.ca>
2018-08-10 16:13:00 -0400

    bug fix

--- utils.py
+++ utils.py
@@ -302,7 +302,7 @@ def load_model_from_file(path, args=None, epoch=None):
     with open(os.path.join(path, 'args.json'), 'r') as f: 
         old_args = json.load(f)
     
-    if args is None: args = get_train_args()
+    if args is None: args = get_train_args(allow_unmatched_args=True)[0]
 
     args_dict = vars(args)
     for key in args_dict.keys():
