4eb59cdeb43e37b7fd18fd37250a0cee4cbddad8
Massimo Caccia <optimass@cedar5.cedar.computecanada.ca>
2018-08-11 15:17:30 -0700

    n/a

--- cc_massimo/cc_launch_news.sh
+++ cc_massimo/cc_launch_news.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1 # request GPU "generic resource" 
 #SBATCH --cpus-per-task=6 # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham. 
-#SBATCH --mem=2000M # memory per
+#SBATCH --mem=10000M # memory per
 #SBATCH --time=0-23:00 # time (DD-HH:MM) 
 #SBATCH --output=logs/%N-%j.out
 #SBATCH --account=rpp-bengioy
